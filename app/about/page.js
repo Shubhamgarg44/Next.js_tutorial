ad576f27fc4fc0d002e037191d7b02e414e25dc4
myprojects <shubhamgar4488@gmail.com>
2024-10-02 05:06:02 +0530

    first commit

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const page = () => {
+    
   return (
     <div>
       i am about
--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
+"use client"
+import { useState, useEffect } from 'react'
 import React from 'react'
+import Navbar from '@/components/Navbar'
 
 const page = () => {
+  const [count, setcount] = useState(0)
   return (
-    <div>
-      i am home page 
+    <div> 
+      <Navbar/>
+      i am home page {count}
+      <button onClick={()=> setcount(count + 1)}> click me</button>
     </div>
   )
 }
